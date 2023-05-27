e0d14e3a47de899248bb35d329d047c8f5304c85
JayadevArun <jayadevarun03@gmail.com>
2023-05-27 12:45:37 +0530

    Added Functions

--- internet_speed.py
+++ internet_speed.py
@@ -25,5 +25,22 @@ root.title("Internet Speed Test")
 root.geometry("500x600")
 root.config(bg="Antiquewhite")
 
+label_heading=Label(root,text="Intenet Speed Test",font=("Arial",30,"bold"),bg="Antiquewhite",fg="Brown")
+label_heading.place(x=50,y=50,height=50,width=400)
+
+label_download=Label(root,text="Download Speed",font=("Arial",30),bg="Antiquewhite",fg="Orange")
+label_download.place(x=50,y=150,height=50,width=400)
+
+label_download_result=Label(root,text="0.0",font=("Arial",30),bg="Antiquewhite",fg="Orange")
+label_download_result.place(x=50,y=200,height=50,width=400)
+
+label_upload=Label(root,text="Upload Speed",font=("Arial",30),bg="Antiquewhite",fg="Orange")
+label_upload.place(x=50,y=300,height=50,width=400)
+
+label_upload_result=Label(root,text="0.0",font=("Arial",30),bg="Antiquewhite",fg="Orange")
+label_upload_result.place(x=50,y=350,height=50,width=400)
+
+button_check=Button(root,text="Check",font=("Arial",30),bg="Antiquewhite",fg="Red",state="normal",borderwidth=5,relief="groove",command=Check)
+button_check.place(x=50,y=450,height=50,width=400)
 
 root.mainloop()
