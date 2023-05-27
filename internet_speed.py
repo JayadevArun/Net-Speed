17efe09e68f3f0ddb6009348cc6f39accaa97d11
JayadevArun <jayadevarun03@gmail.com>
2023-05-27 12:39:59 +0530

    Structure Added

--- internet_speed.py
+++ internet_speed.py
@@ -1,4 +1,24 @@
 from tkinter import *
+
+import speedtest
+
+def Check():
+    
+    try:
+        
+        root=speedtest.Speedtest()
+        root.get_servers()
+
+        download=str(round(root.download()/(10**6),3))+" Mbps"
+        upload=str(round(root.upload()/(10**6),3))+" Mbps"
+
+        label_download_result.config(text=download)
+        label_upload_result.config(text=upload)
+        
+    except:
+        
+        print("Not connected to internet!")
+        
 root=Tk()
 
 root.title("Internet Speed Test")
